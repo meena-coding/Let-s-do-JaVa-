0a46db4e3cb39ac729c268902bb9edb496bbc45e
Aryan Kankarwal <aryankankarwal9714@gmail.com>
2025-06-21 22:35:01 +0530

    new lecture started

--- Lecture codes!/11 collection and generic/Lecture/src/in/MeenaCoding/VarArgs/VarArgs.java
+++ Lecture codes!/11 collection and generic/Lecture/src/in/MeenaCoding/VarArgs/VarArgs.java
@@ -1,7 +1,31 @@
 package in.MeenaCoding.VarArgs;
 
 public class VarArgs {
-    public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+    public static void main(String... args) {
+        System.out.printf("sum of the Number is: %d",sum(4,3,2,4,5,633,33,4,4,3,2));
+        System.out.printf("sum of the Number is: %d",sum(4,3,2,4,5,633,33,4,4,3,2));
+
+
+
+        }
+
+        public static int sum (int first, int second, int... a){
+            int sum = first + second;
+
+            for (int i : a) {
+                sum += i;
+            }
+            return sum;
+        }
+
+//    public static int sum(int[] a){
+//        int sum = 0;
+//
+//        for (int i : a) {
+//            sum += i;
+//        }
+//        return sum;
+//
+//    }
     }
 }
