7c63328aba48b024045b32d20ca1e643889a32fa
Aryan Kankarwal <aryankankarwal9714@gmail.com>
2025-07-21 18:33:25 +0530

    Adding problems

--- LeetCode Problems/1 Leet Code Easy Problems/src/test.java
+++ LeetCode Problems/1 Leet Code Easy Problems/src/test.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class test {
 
-    public static ArrayList<Integer> commonElements(int a[], int b[]) {
+    public static ArrayList<Integer> commonElements(int[] a, int[] b) {
         // Your code here
         ArrayList<Integer> result = new ArrayList<>();
-
-
+        result.add(4);
+        System.out.println(result);
         return null;
 
 
